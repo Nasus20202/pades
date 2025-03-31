388aab440a6241a64e01b8dac13e76fcc6a77754
fildaw <filippo1056@gmail.com>
2025-03-31 11:15:50 +0200

    feat: configure doxygen, add docs for crypt and key_mamagement modules

--- lib/crypt.py
+++ lib/crypt.py
@@ -1,13 +1,30 @@
+"""!
+@package crypt
+This module contains cryptographic functions for hashing, RSA key generation, AES encryption and decryption.
+"""
+
 import Crypto.PublicKey.RSA as RSA
 import Crypto.Hash.SHA256 as SHA256
 import Crypto.Cipher.AES as AES
 
 
 def hash_string(string: str) -> str:
+    """!
+    Hash a string using SHA256.
+
+    @param string: The string to hash.
+
+    @return The hashed string."""
     return SHA256.new(string.encode("utf-8")).digest()
 
 
 def generate_rsa_key_pair(key_size: int = 4096) -> tuple[bytes, bytes]:
+    """!
+    Generate a RSA key pair.
+
+    @param key_size: The size of the key in bits.
+
+    @return A tuple containing the private and public key."""
     key = RSA.generate(key_size)
     private_key = key.export_key()
     public_key = key.publickey().export_key()
@@ -15,6 +32,13 @@ def generate_rsa_key_pair(key_size: int = 4096) -> tuple[bytes, bytes]:
 
 
 def encrypt_data_with_aes(data: bytes, key: bytes) -> bytes:
+    """!
+    Encrypt data using AES.
+
+    @param data: The data to encrypt.
+    @param key: The key to use for encryption.
+
+    @return The encrypted data."""
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(data)
     return cipher.nonce, tag, ciphertext
@@ -23,15 +47,32 @@ def encrypt_data_with_aes(data: bytes, key: bytes) -> bytes:
 def decrypt_data_with_aes(
     nonce: bytes, tag: bytes, ciphertext: bytes, key: bytes
 ) -> bytes:
+    """!
+    Decrypt data using AES.
+
+    @param nonce: The nonce used for encryption.
+    @param tag: The tag used for encryption.
+    @param ciphertext: The encrypted data.
+    @param key: The key to use for decryption.
+
+    @return The decrypted data."""
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     return cipher.decrypt_and_verify(ciphertext, tag)
 
 
 def merge_cipher_data(nonce: bytes, tag: bytes, ciphertext: bytes) -> bytes:
+    """!
+    Merge the nonce, tag and ciphertext into a single byte string.
+
+    @return The merged data."""
     return nonce + tag + ciphertext
 
 
 def split_cipher_data(data: bytes) -> tuple[bytes, bytes, bytes]:
+    """!
+    Split the merged data into the nonce, tag and ciphertext.
+
+    @return A tuple containing the nonce, tag and ciphertext."""
     nonce = data[:16]
     tag = data[16:32]
     ciphertext = data[32:]
--- lib/key_management.py
+++ lib/key_management.py
@@ -1,15 +1,38 @@
+"""!
+@package key_management
+This module contains functions related to key management.
+"""
+
 import os
 
 from lib.crypt import *
 
+## @var PRIVATE_KEY_DIR
+# The directory where the private key is stored on the device.
 PRIVATE_KEY_DIR = ""
+## @var PRIVATE_KEY_FILENAME
+# The filename of the private key.
 PRIVATE_KEY_FILENAME = "private_key.pem"
+## @var ENCRYPTED_EXTENSION
+# The extension of the encrypted private key.
 ENCRYPTED_EXTENSION = ".aes"
-PUBLIC_KEY_DIR = os.getcwd()
+## @var PUBLIC_KEY_DIR
+# The directory where the public key is stored (current working directory).
+PUBLIC_KEY_DIR: str = os.getcwd()
+## @var PUBLIC_KEY_FILENAME
+# The filename of the public key.
 PUBLIC_KEY_FILENAME = "public_key.pub"
 
 
 def generate_and_save_keys(device_path: str, pin: str) -> tuple[str, str]:
+    """!
+    Generate a RSA key pair and save it to the @ref PUBLIC_KEY_DIR and the private key to the device_path.
+
+    @param device_path: The path to the device where the private key should be saved.
+    @param pin: The pin to encrypt the private key with.
+
+    @return A tuple containing the path to the private key and the path to the public key.
+    """
     private_key, public_key = generate_rsa_key_pair()
     return (
         encrypt_and_save_private_key(pin, device_path, private_key),
@@ -17,7 +40,16 @@ def generate_and_save_keys(device_path: str, pin: str) -> tuple[str, str]:
     )
 
 
-def encrypt_and_save_private_key(pin: str, device_path: str, private_key: bytes):
+def encrypt_and_save_private_key(pin: str, device_path: str, private_key: bytes) -> str:
+    """!
+    Encrypt the private key with the pin and save it to the device_path.
+
+    @param pin: The pin to encrypt the private key with.
+    @param device_path: The path to the device where the private key should be saved.
+    @param private_key: The private key to encrypt and save.
+
+    @return The path to the private key.
+    """
     aes_key = hash_string(pin)
     private_key_nonce, private_key_tag, encrypted_private_key = encrypt_data_with_aes(
         private_key, aes_key
@@ -36,7 +68,14 @@ def encrypt_and_save_private_key(pin: str, device_path: str, private_key: bytes)
     return private_key_path
 
 
-def save_public_key(public_key: bytes):
+def save_public_key(public_key: bytes) -> str:
+    """!
+    Save the public key to the @ref PUBLIC_KEY_DIR under @ref PUBLIC_KEY_FILENAME name.
+
+    @param public_key: The public key to save.
+
+    @return The path to the public key.
+    """
     public_key_path = f"{PUBLIC_KEY_DIR}/{PUBLIC_KEY_FILENAME}"
     with open(
         public_key_path,
@@ -47,6 +86,15 @@ def save_public_key(public_key: bytes):
 
 
 def read_and_decrypt_private_key(pin: str, device_path: str) -> bytes:
+    """!
+    Read the private key from the device_path and decrypt it with the pin.
+    The filename of the private key is @ref PRIVATE_KEY_FILENAME with @ref ENCRYPTED_EXTENSION.
+
+    @param pin: The pin to decrypt the private key with.
+    @param device_path: The path to the device where the private key is stored.
+
+    @return The decrypted private key or None when there is an error opening the private key file.
+    """
     aes_key = hash_string(pin)
     with open(
         f"{device_path}/{PRIVATE_KEY_DIR}{PRIVATE_KEY_FILENAME}{ENCRYPTED_EXTENSION}",
@@ -63,6 +111,14 @@ def read_and_decrypt_private_key(pin: str, device_path: str) -> bytes:
 
 
 def read_public_key(path: str = None) -> bytes:
+    """!
+    Read the public key from the path.
+    The default path is @ref PUBLIC_KEY_DIR with @ref PUBLIC_KEY_FILENAME.
+
+    @param path: The path to the public key.
+
+    @return The public key or None when there is an error opening the public key file.
+    """
     if not path:
         path = f"{PUBLIC_KEY_DIR}/{PUBLIC_KEY_FILENAME}"
     with open(path, "rb") as file:
@@ -71,6 +127,13 @@ def read_public_key(path: str = None) -> bytes:
 
 
 def check_if_directory_contains_keys(device_path: str) -> bool:
+    """!
+    Check if the device_path contains the private key and the @ref PUBLIC_KEY_DIR contains the public key with @ref PUBLIC_KEY_FILENAME.
+
+    @param device_path: The path to the device.
+
+    @return True if both files exist, False otherwise.
+    """
     return os.path.exists(
         f"{device_path}/{PRIVATE_KEY_DIR}{PRIVATE_KEY_FILENAME}{ENCRYPTED_EXTENSION}"
     ) and os.path.exists(f"{PUBLIC_KEY_DIR}/{PUBLIC_KEY_FILENAME}")
